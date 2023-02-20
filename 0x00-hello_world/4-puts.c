821981d7b99a47e4aa4851a6e5168227ec007e1b
B-Friend <munyawera123456789@gmail.com>
2023-02-19 23:29:56 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-/* betty guidance on how to create c program*/
 /**
-*main A program that print a line with puts function
+ * main -entry point where c program start execution
+ * main -  a program that print a line with puts function
 *return: 0 (success)
  */
+/* start of main function */
 int main(void)
 {
+/*out put of c function using puts*/
 	puts("\"Programming is like building a multilingual puzzle");
+	/* return 0 if the program give 0 errors*/
 	return (0);
+/*main : end  of main program and of a program*/
 }
 
