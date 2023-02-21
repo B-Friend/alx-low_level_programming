fb9f8da93ae416767686a15a1ec1e17acb97172e
B-Friend <munyawera123456789@gmail.com>
2023-02-20 19:36:59 -0800

    print decimal10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int a;
+
+	a == 0;
+	while
+		(a < 10) {
+			putchar(a);
+			putchar('\n');
+			a++;
+		}
+	return (0);
+}
