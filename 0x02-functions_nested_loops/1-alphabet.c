c4e3b260a1584726f4d7e132d56d9c9e12f580ea
B-Friend <munyawera123456789@gmail.com>
2023-02-22 19:32:00 -0800

    nothingeasy' git commit -m nothingeasy git commit -m "nothingeasy

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,13 @@
  * main - check the code
  *
  * Return: Always 0.
+ * print-alphabet - Print the alphabet in lowercase
  */
 void print_alphabet(void)
 {
-	int i;	
-	
-	for(i = 97; i < = 122; i++)
+int i;
+
+	for (i = 97; i < = 122; i++)
 	{
 		_putchar(i);
 	}
