e3eb77e386288a82ec19533179d73df821f4e496
B-Friend <munyawera123456789@gmail.com>
2023-02-26 20:23:30 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
  * main - check the code
  *
  * Return: Always 0.
- * print-alphabet - Print the alphabet in lowercase
+ * function that prints the alphabet
  * No return:value
  */
 void print_alphabet(void)
 {
 int i;
 
-	for (i = 'a'; i < = 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,16 +4,19 @@
  * main - check the code.
  *
  * Return: Always 0.
+ * _is alpha -Shows 1 if  the input is 1 
+ * another case is 0
+ * Return : 1 for letters 0 for others
  */
 int _isalpha(int c)
 {
-	if (( c >= 97 && c <=122 c <= 122) || c > 65 && c >= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (1);
-	}
-	_putchar('\n');
+if ((c >= 97 && c <= 122 c <= 122) || c > 65 && c >= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
 }
