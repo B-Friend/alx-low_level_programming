286385c9c2a2325f16b9f5651d81de1dbf01349b
B-Friend <munyawera123456789@gmail.com>
2023-02-28 02:17:22 -0800

    swapping

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void reset_to_98(int *n);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
@@ -8,3 +11,5 @@ void print_rev(char *s);
 void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
+#endif
+
