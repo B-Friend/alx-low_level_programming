543df512c0a1dece270b47f3516e44959675a4c2
B-Friend <munyawera123456789@gmail.com>
2023-03-08 11:29:01 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*factorial ->  function facto
+*@n: number
+*Return: Always 0 (success)
+*/
+int factorial(int n)
+{
+
+if (n < 0)
+return (-1);
+if (n <= 1)
+return (1);
+return (n * factorial(n - 1));
+}
