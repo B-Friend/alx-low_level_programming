c1765b2e20a0924bebbf2bd440c4bb8361830b7c
B-Friend <munyawera123456789@gmail.com>
2023-03-10 12:23:55 -0800

    argc&&argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main -> This is function that print it's namey
+*@argc: argc parameter
+*@argv: argv an array of a command listed
+*Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
